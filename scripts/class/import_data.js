016a7cb6a22e18d4446c5edeefb3ff74856f3233
Alex Mant <al.mantione@gmail.com>
2021-12-30 17:34:34 +0100

    tests

---
+++ scripts/class/import_data.js
@@ -0,0 +1,7 @@
+// const fisheyeData = '../../data/photographers.json'
+
+// const listOfPhotographers = [];
+
+// fetch(fisheyeData)
+//     .then(blob => blob.json())
+//     .then(data => listOfPhotographers.push(...data.photographers));
