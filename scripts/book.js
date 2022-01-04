e11e621914019f161ec7729de7130ba599e62759
Alex Mant <al.mantione@gmail.com>
2022-01-04 21:37:45 +0100

    début fonction affichage gallerybook

--- scripts/book.js
+++ scripts/book.js
@@ -1,5 +1,6 @@
 import { openForm, closeForm, submitForm } from "./utils/modalForm.js";
 import { getSelectValue } from "./utils/galleryFilter.js"
+import { Mydata } from "./utils/class_Mydata.js";
 
 
 /* OUVERTURE ET FERMETURE DU FORMULAIRE */
@@ -8,3 +9,20 @@ closeForm();
 submitForm();
 /* FILTRE GALLERY*/
 getSelectValue();
+
+/* */
+
+const mediaDisplay = async () => {
+    const data = new Mydata();
+    await data.initData();
+   
+
+    // document.querySelector(".mediaGallery").innerHTML = 
+    //     `
+    //     <img class="media-img" src="${allMedia.image}"></img>
+        
+    //     `
+    // ).join(""));
+
+}
+mediaDisplay();
