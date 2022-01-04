e7cb2a926b2046587f986f38c8c169571b61ab38
Alex Mant <al.mantione@gmail.com>
2022-01-05 00:08:37 +0100

    ajustage css et implémentation de la partie bio de la page book

--- scripts/book.js
+++ scripts/book.js
@@ -19,11 +19,27 @@ const mediaDisplay = async () => {
     const media = data.getAllMedia();
     
     document.querySelector(".photograph-book_portrait").src =`${photographer.portrait}`;
+    document.querySelector(".photograph-book_name").innerHTML=`${photographer.name}`;
+    document.querySelector(".photograph-book_location").innerHTML=`${photographer.city}, ${photographer.country}`;
+    document.querySelector(".photograph-book_tagline").innerHTML=`${photographer.tagline}`;
     
     document.querySelector(".mediaGallery").innerHTML = media.map((media) =>
     `
-    <img class="media-img" src="/assets/albums/${photographer.name.split(" ")[0]}/${media.image}"></img>    
-    
+    <figure class="gallery-cards">
+        <a href="/assets/albums/${photographer.name.split(" ")[0]}/${media.image}">
+        <img class="media-img" src="/assets/albums/${photographer.name.split(" ")[0]}/${media.image}">
+        </a>
+
+        <figcaption class="media-titlesnlikes">
+            <span class="media-title">${media.title}</span>
+            <span class="media-likes">
+                ${media.likes}
+                <button>
+                    <i class="fas fa-heart"></i>
+                </button>
+            </span>
+        </figcaption>  
+    </figure>
     `
     ).join("")    
 
