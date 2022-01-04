39c8427e7584e9f009a6852cff20246c1b7b3d03
Alexandre <89981887+Alex-mant@users.noreply.github.com>
2022-01-05 00:11:41 +0100

    Update book.js

--- scripts/book.js
+++ scripts/book.js
@@ -26,7 +26,7 @@ const mediaDisplay = async () => {
     document.querySelector(".mediaGallery").innerHTML = media.map((media) =>
     `
     <figure class="gallery-cards">
-        <a href="/assets/albums/${photographer.name.split(" ")[0]}/${media.image}">
+        <a href="./assets/albums/${photographer.name.split(" ")[0]}/${media.image}">
         <img class="media-img" src="/assets/albums/${photographer.name.split(" ")[0]}/${media.image}">
         </a>
 
