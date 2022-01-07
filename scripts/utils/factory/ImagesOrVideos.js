857014c98d729ef1e0a59a173d001ee6c636e9e9
Alex Mant <al.mantione@gmail.com>
2022-01-07 16:16:11 +0100

    préparation fonction like et incrémentation des likes

--- scripts/utils/factory/ImagesOrVideos.js
+++ scripts/utils/factory/ImagesOrVideos.js
@@ -1,49 +1,48 @@
 export const factoryGallery = (medias, photographer) => {
   const createMediaEnvironnement = (media) => {
     const name = photographer.name.split(" ")[0];
-    /*Création des élements*/
-    const figure = document.createElement("figure");
     let image;
     let video;
     if (media.image) {
+      /*Création des élements*/
       image = document.createElement("img");
+      /*Assignation des attributs*/
+      image.setAttribute("src", `./assets/albums/${name}/${media.image}`);
+      MediaLink.setAttribute("href", `./assets/albums/${name}/${media.image}`);
+      /*Assignation de classes css*/
+      image.classList.add("media-imgnvideo");
+      /*RATTACHEMENT des elements*/
+      MediaLink.append(image);
     } else if (media.video) {
+      /*Création des élements*/
       video = document.createElement("video");
+      /*Assignation des attributs*/
+      video.setAttribute("src", `./assets/albums/${name}/${media.video}`);
+      MediaLink.setAttribute("href", `./assets/albums/${name}/${media.video}`);
+      /*Assignation de classes css*/
+      video.classList.add("media-imgnvideo");
+      /*RATTACHEMENT des elements*/
+      MediaLink.append(video);
     }
+    /*Création des élements*/
+    const figure = document.createElement("figure");
     const MediaLink = document.createElement("a");
     const figCaption = document.createElement("figcaption");
     const span1 = document.createElement("span");
-    const span2 = document.createElement("span");
-    /*Assignation des attributs*/
-    if (media.image) {
-      image.setAttribute("src", `./assets/albums/${name}/${media.image}`);
-    } else if (media.video) {
-        video.setAttribute("src", `./assets/albums/${name}/${media.video}`);
-    }
-    MediaLink.setAttribute("href", `./assets/albums/${name}/${media.image}`);
+    const span2 = document.createElement("span");   
 
     /*Assignation de classes css*/
     figure.classList.add("gallery-cards");
-    if (media.image) {
-      image.classList.add("media-imgnvideo");
-    } else if (media.video) {
-      video.classList.add("media-imgnvideo");
-    }
     figCaption.classList.add("media-titlesnlikes");
     span1.classList.add("media-title");
     span2.classList.add("media-likes");
     /* Ajout dans l'HTML */
     span1.innerHTML = media.title;
-    span2.innerHTML = `${media.likes} <button role="button" aria-label="likes" class="btn--work"><i aria-hidden="true" class="fas fa-heart"></i></button>`;
+    span2.innerHTML = `${media.likes} <button role="button" aria-label="likes" class="btn-for-"><i aria-hidden="true" class="fas fa-heart"></i></button>`;
     /*RATTACHEMENT des elements*/
     document.querySelector(".mediaGallery").append(figure);
     figure.append(figCaption, MediaLink);
     figCaption.append(span1, span2);
-    if (media.image) {
-      MediaLink.append(image);
-    } else if (media.video) {
-      MediaLink.append(video);
-    }
   };
  
   const createImage = (media) => {
