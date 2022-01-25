713625ad87f169d204e10ffbf5baaedcb95b22da
Alex Mant <al.mantione@gmail.com>
2022-01-25 13:31:46 +0100

    fix name of functon (rename forget) / mediaDisplay to display

--- scripts/utils/fonctions/refreshGallery.js
+++ scripts/utils/fonctions/refreshGallery.js
@@ -1,6 +1,6 @@
 export const refreshGallery = (triggersTheGalleryRefreshment, Gallery) => {
     triggersTheGalleryRefreshment.addEventListener("change", () => {
         const resetGallery = document.querySelector(".mediaGallery").innerHTML = "";
-        Gallery.mediaDisplay();
+        Gallery.display();
     });
 }
