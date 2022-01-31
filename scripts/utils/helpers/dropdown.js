70a27ed26f1282552b0d9f34559fed6cb95acf45
Alex Mant <al.mantione@gmail.com>
2022-01-31 12:09:44 +0100

    test new filter

--- scripts/utils/helpers/dropdown.js
+++ scripts/utils/helpers/dropdown.js
@@ -1,32 +1,146 @@
 import { getRuleWithSelector } from "./getRulesWithSelector.js";
 
-const selected = document.querySelector(".selected");
-const optionsContainer = document.querySelector(".options-container");
-const selectBox = document.querySelector("select-box");
-const selectedAfterStyle = getRuleWithSelector(".selected::after");
-const optionsList = document.querySelectorAll(".option");
+const filterContainer = document.querySelector(".filter-container");
+const optContainer = document.querySelector(".options-container");
+const dropDownArrow = document.querySelector(".arrow");
+const currentOption = document.querySelector(".current-option");
+const opt2 = document.querySelector(".opt2");
+const opt3 =document.querySelector(".opt3");
 
-selected.addEventListener("click", () => {
-    optionsContainer.classList.toggle("opt-active");
-    selected.classList.toggle("sel-radius");
-    selectedAfterStyle.style.transform = "rotate(270deg)"
 
-    
+currentOption.addEventListener("click", () => {
+    optContainer.classList.toggle("dropdown");
+    dropDownArrow.classList.toggle("reverseArrow");
+    filterContainer.classList.toggle("position-fix");
 });
 
-optionsList.forEach((o) => {
-    o.addEventListener("click", () => {
-        let options = Array.from(o.parentElement.children);
-        options.map(option => {
-            if(option.classList.contains("disNone")){
-                option.classList.remove("disNone");
-            }
-        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const selected = document.querySelector(".selected");
+// const optionsContainer = document.querySelector(".options-container");
+// const selectedAfterStyle = getRuleWithSelector(".selected::after");
+// const optionsList = document.querySelectorAll(".option");
+
+// selected.addEventListener("click", () => {
+//     optionsContainer.classList.toggle("opt-active");
+//     selected.classList.toggle("sel-radius");
+//     selectedAfterStyle.style.transform = "rotate(270deg)"
+// });
+
+// optionsList.forEach((o) => {
+//     o.addEventListener("click", () => {
+//         let options = Array.from(o.parentElement.children);
+//         options.map(option => {
+//             if(option.classList.contains("disNone")){
+//                 option.classList.remove("disNone");
+//             }
+//         });
                         
-        selected.innerHTML = o.querySelector("label").innerHTML;
-        optionsContainer.classList.remove("opt-active");
-        selected.classList.toggle("sel-radius");
-        o.classList.add("disNone");
-        selectedAfterStyle.style.transform = "rotate(90deg)"      
-    });
-});
+//         selected.innerHTML = o.querySelector("label").innerHTML;
+//         optionsContainer.classList.remove("opt-active");
+//         selected.classList.toggle("sel-radius");
+//         o.classList.add("disNone");
+//         selectedAfterStyle.style.transform = "rotate(90deg)"      
+//     });
+// });
